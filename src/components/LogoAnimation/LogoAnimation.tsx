4cd2abdcfeda3947bfebffdb0182b08ce4c330b9
Rostik Wozniak <rostik19wozniak@icloud.com>
2024-03-23 18:48:32 +0100

    fix loader

--- src/components/LogoAnimation/LogoAnimation.tsx
+++ src/components/LogoAnimation/LogoAnimation.tsx
@@ -5,7 +5,6 @@ import "./style.css";
 const LogoAnimation = () => {
 
   const [isLoaderVisible, setIsLoaderVisible] = useState(true);
-  const [animate, setAnimate] = useState(true);
 
   const delayLoading = 3000;
   useEffect(() => {
@@ -17,8 +16,6 @@ const LogoAnimation = () => {
 
   const delay = delayLoading / 1000 - 1;
   return (
-   <>
-   {animate && (
      <div
       onTouchStart={(e) => e.stopPropagation()}
       className="z-[100] bg-[url('/src/assets/images/background/home-bg.jpg')] bg-gray bg-cover bg-center bg-no-repeat w-full h-full"
@@ -30,10 +27,6 @@ const LogoAnimation = () => {
     >
       <Logo />
     </div>
-   )}
-    
-    </>
-
   );
 };
 
