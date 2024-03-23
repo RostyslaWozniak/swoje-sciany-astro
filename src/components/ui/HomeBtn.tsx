559d73d49889eed97dd27137f1458bf936e0730f
Rostik Wozniak <rostik19wozniak@icloud.com>
2024-03-23 12:14:26 +0100

    change style css add shdow and ohters...

--- src/components/ui/HomeBtn.tsx
+++ src/components/ui/HomeBtn.tsx
@@ -8,7 +8,7 @@ export const HomeBtn = () => {
   return(       
     <button
      onClick={() => setIsNumberVisible(true)}
-        className="relative bg-accent/70 w-[min(400px,100%)] flex flex-col items-center  rounded-full border-2 h-14 font-bold text-2xl overflow-hidden [text-shadow:_1px_1px_0_rgb(5_5_5_)] hover:bg-accent/50 duration-300"
+        className="relative bg-accent/70 w-[min(400px,100%)] flex flex-col items-center  rounded-full border-2 h-14 font-bold text-2xl overflow-hidden [text-shadow:_1px_1px_0_rgb(5_5_5_)] hover:bg-accent/50 duration-300 shadow-castom"
         >
             <div className={cn("h-20 top-0 leading-[50px]   absolute", {"-top-14": isNumberVisible})}>Skontaktuj się z nami</div>
             <a href={`tel:${phoneNumber}`} className={cn("transition-all w-full -bottom-14 leading-[50px] absolute", {"bottom-0": isNumberVisible})}>{phoneNumber}</a>
