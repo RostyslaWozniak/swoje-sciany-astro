bba2f4bd53c18a3887b1c8ec33fc41cc3788ea6b
Rostik Wozniak <rostik19wozniak@icloud.com>
2024-03-23 18:39:34 +0100

    add loader

---
+++ src/components/LogoAnimation/LogoAnimation.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import image from "@assets/images/background/home-bg.jpg";
+import Logo from "./Logo";
+import "./style.css";
+
+const LogoAnimation = () => {
+
+  const [isLoaderVisible, setIsLoaderVisible] = useState(true);
+  const [animate, setAnimate] = useState(true);
+
+  const delayLoading = 3000;
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoaderVisible(false);
+      document.body.style.overflow = "auto";
+    }, delayLoading);
+  }, [delayLoading]);
+
+  const delay = delayLoading / 1000 - 1;
+  return (
+   <>
+   {animate && (
+     <div
+      onTouchStart={(e) => e.stopPropagation()}
+      className="bg-[url('/src/assets/images/background/home-bg.jpg')] bg-gray bg-cover bg-center bg-no-repeat w-full h-full"
+      id="loader-container"
+      style={{
+        display: isLoaderVisible ? "grid" : "none",
+        animation: `2s ${delay}s  linear bg-disappear`,
+      }}
+    >
+      <Logo />
+    </div>
+   )}
+    
+    </>
+
+  );
+};
+
+export default LogoAnimation;
