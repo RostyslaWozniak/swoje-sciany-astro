0db3d0756a940e26a1567f1ff9808b3e2bdb2d93
Rostik Wozniak <rostik19wozniak@icloud.com>
2024-04-18 22:02:47 +0200

    fix loader bg

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,17 +3,20 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      colors:{
-        primary: "#909091",
+      colors: {
+        primary: "#898989",
         secondary: "#6C6C6B",
         dark: "#434343",
         light: "#B4B4B4",
-        accent: "#ec7a1c"
+        accent: "#ec7a1c",
       },
       boxShadow: {
-        castom: "0px 0px 30px 1px rgba(0,0,0,0.4)"
-      }
+        castom: "0px 0px 30px 1px rgba(0,0,0,0.4)",
+      },
     },
   },
-  plugins: [require("tailwind-scrollbar-hide"), require('@tailwindcss/typography')],
+  plugins: [
+    require("tailwind-scrollbar-hide"),
+    require("@tailwindcss/typography"),
+  ],
 };
