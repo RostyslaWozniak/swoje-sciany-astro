d3a75b9bcf335bb39b083a51d96f84c5c081d1fb
Rostyslav Vozniak <rostik19wozniak@icloud.com>
2024-07-01 13:31:57 +0200

    fix build errors

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -2,7 +2,6 @@ import { LayoutGrid } from "@components/gridLayout/LayoutGrid";
 import { gridImages, scrollImages } from "@components/gridLayout/data/data";
 import { useEffect, useState } from "react";
 import { ScrollGallery } from "./ScrollGallery";
-import H2 from "./ui/H2.astro";
 
 export const ImageGallery = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
--- src/components/ScrollGallery.tsx
+++ src/components/ScrollGallery.tsx
@@ -1,32 +1,23 @@
-import type { Image } from "./gridLayout/LayoutGrid";
-
 type ScrollGalleryProps = {
   images: {
-    
     format: string;
     width: number;
     height: number;
     src: string;
-  }[]
-}
+  }[];
+};
 
-export const ScrollGallery = ({images}:  ScrollGalleryProps) => {
-  return ( <div
-  className="flex snap-x snap-mandatory items-center gap-5 overflow-x-scroll px-2.5 py-12 scrollbar-hide"
->
-  {
-    images.map(({src}, i) => (
-      <img
-        key={i}                       
-        src={src}
-        alt="zdjęcie realizacji remontu"
-        className="h-full w-[90vw] snap-center rounded-xl shadow-xl shadow-black/30 sm:w-[300px]"
-      />
-    ))
-  }
-</div> );
-}
- 
-
-
-
+export const ScrollGallery = ({ images }: ScrollGalleryProps) => {
+  return (
+    <div className="flex snap-x snap-mandatory items-center gap-5 overflow-x-scroll px-2.5 py-12 scrollbar-hide">
+      {images.map(({ src }, i) => (
+        <img
+          key={i}
+          src={src}
+          alt="zdjęcie realizacji remontu"
+          className="h-full w-[90vw] snap-center rounded-xl shadow-xl shadow-black/30 sm:w-[300px]"
+        />
+      ))}
+    </div>
+  );
+};
